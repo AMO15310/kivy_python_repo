4dfacb838a5d92714b7361e689980c28ed5d2881
AMO15310 <wachiraamos402@gmail.com>
2022-07-21 23:58:36 +0300

    display an image

---
+++ image.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.image import Image
+
+class MainApp(App):
+    def build(self):
+        image = Image(source='/path/to/your/image.jpg',
+                size_hint = (1,.5),
+                pos_hint={'center_x':.5,'center_y':.5})
+        return image
+if __name__=="__main__":
+    app = MainApp()
+    app.run()
